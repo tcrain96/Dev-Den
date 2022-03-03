1ae86f97bf119d4570be27c6c0790fbfe8ed4cb5
Tristan Crain <tcrain@metbiblechurch.local>
2022-03-03 14:43:49 -0500

    added my posts page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     Post.findAll({
@@ -20,7 +21,8 @@ router.get('/', (req, res) => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
         res.render('homepage', {
           posts,
-          loggedIn: req.session.loggedIn
+          loggedIn: req.session.loggedIn,
+          userId:req.session.user_id
         });
       })
       .catch(err => {
@@ -37,6 +39,37 @@ router.get('/login', (req, res) => {
   
     res.render('login');
 });
+
+router.get('/posts/:id',withAuth,(req,res)=>{
+  Post.findAll({
+    where: {
+      user_id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'post_text',
+      'user_id'
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('my-posts', {
+        posts,
+        loggedIn: req.session.loggedIn,
+        userId:req.session.user_id
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+})
 
 router.get('/post/:id',(req,res)=>{
   Post.findOne({
